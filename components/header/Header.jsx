fc005a5bf45a5f07e60a11206cb498a973b0f8f8
AnshukGottipati <anshukgp18@gmail.com>
2023-10-01 23:23:48 -0400

    update 1

---
+++ components/header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css';
+
+class Header extends Componenet{
+    render(){
+        return (
+            <section>
+                Hello
+            </section>
+
+        );
+
+    }
+}
+export default header;
